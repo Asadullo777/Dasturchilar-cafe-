8c3dd6d405a4518da8ea095197ce70e97280705a
Asadullo <asadullon768@gmail.com>
2023-03-21 18:18:53 +0500

    I created right banner for use search input

--- src/components/RightBanner/RightBanner.jsx
+++ src/components/RightBanner/RightBanner.jsx
@@ -4,6 +4,8 @@ import styles from './RightBanner.module.scss'
 
 export const RightBanner = () => {
   return (
-    <div className={styles.rightBanner}>RightBanner</div>
+    <div className={styles.rightBanner}>
+      <input className={styles.input} placeholder="Поиск ..." type="search" />
+    </div>
   )
 }
--- src/pages/DiscussionsPage.jsx
+++ src/pages/DiscussionsPage.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import styles from "./Index.module.scss"
-import { CenterBanner } from "../components/CenterBanner/CenterBanner";
 import { LeftBanner } from "../components/LeftBanner/LeftBanner";
 import { RightBanner } from "../components/RightBanner/RightBanner";
 
