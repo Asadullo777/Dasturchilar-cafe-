83c29393e0adab4d2768b94ec60a36c658bee9c0
Asadullo <asadullon768@gmail.com>
2023-03-22 13:01:49 +0500

    I created only  Ui/Ux do not use json file only Ui

--- src/components/CenterBanner/CenterBanner.jsx
+++ src/components/CenterBanner/CenterBanner.jsx
@@ -1,7 +1,28 @@
 import React from 'react'
+import styles from "./CenterBanner.module.scss"
 
 export const CenterBanner = () => {
   return (
-    <div>CenterBanner</div>
+    <div>
+      <h1>Блоги</h1>
+      <div className={styles.card}>
+          <img className={styles.card_img}  src="/image.png" alt="" />
+
+          <div className={styles.card_base}>
+              <div className={styles.card_contact}>
+                <img   src="/user.svg" alt="" />
+                <div className={styles.card_contact_user}>Ismailov Yakhyo</div>
+              </div>
+           
+          <div className={styles.time}>23.12.2021</div>
+
+          </div>
+          <div className={styles.card_subtxt}>
+          Best design 🎨 resources websites every developer 💻 should bookmark🔖 
+          </div>
+
+         
+      </div>
+    </div>
   )
 }
